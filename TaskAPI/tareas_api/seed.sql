3dfbc80cbad80064f05d4ac0551f65b8879ab8c2
xxxxRONALDxxxx <marbinmeneses@unicomfacauca.edu.co>
2026-03-24 12:34:57 -0500

    Delete TaskAPI directory

--- TaskAPI/tareas_api/seed.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO tareas (usuario_id, titulo, descripcion, completada, intervalo_notificacion)
-VALUES (1, 'Verificacion de notificaciones', 'Verificar si la notificacion llega a ronald', 0, 1);
