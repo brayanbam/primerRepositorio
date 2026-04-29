62dba6ed2e58f4a133f60f7c64899ce1105dd20c
xxxxRONALDxxxx <marbinmeneses@unicomfacauca.edu.co>
2026-04-29 12:54:16 -0500

    Add files via upload

---
+++ TaskAPI/notificaciones_api/config.py
@@ -0,0 +1,8 @@
+USUARIOS_API = "http://usuarios:5001/usuarios"
+TAREAS_API = "http://tareas:5002/tareas"
+TELEGRAM_TOKEN = "7797190405:AAE21_1Op0hFoHjD27VoF_t827YANKUi2ZU"
+
+# Token especial para la API de notificaciones, solo esta API lo usará
+NOTIFICACIONES_TOKEN = "TOKEN-NOTIF"
+PORT = 5003
+ 
